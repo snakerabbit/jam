84c44dab72e72fa846226568c0c6b4da585596ee
snakerabbit <alison.s.cheng@gmail.com>
2017-09-20 21:55:23 -0700

    troubleshoot bgimage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
   config.serve_static_assets = true
   config.assets.compile = true
+  config.assets.digest = true
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
