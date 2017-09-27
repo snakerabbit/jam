e9fca28afd44bd82a301bb285183ce47311ce358
snakerabbit <alison.s.cheng@gmail.com>
2017-09-27 08:08:29 -0700

    added questions to index container

--- frontend/components/users/user_index_container.js
+++ frontend/components/users/user_index_container.js
@@ -1,10 +1,12 @@
 import { connect } from 'react-redux';
 import UserIndex from './user_index';
 import { fetchUsers, fetchUser, fetchAllUsers} from '../../actions/user_actions';
+import { fetchQuestions } from '../../actions/question_actions';
 
 const mapStateToProps = state => {
   return({
-    users: Object.values(state.users)
+    users: Object.values(state.users),
+    questions: state.questions.questions
   });
 };
 
@@ -12,7 +14,8 @@ const mapDispatchToProps = dispatch => {
   return({
     fetchAllUsers: () => dispatch(fetchAllUsers()),
     fetchUsers:(instrument) => dispatch(fetchUsers(instrument)),
-    fetchUser: id => dispatch(fetchUser(id))
+    fetchUser: id => dispatch(fetchUser(id)),
+    fetchQuestions: () => dispatch(fetchQuestions())
   });
 };
 
