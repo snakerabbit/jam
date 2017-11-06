3b2cfdb9812ba642899be075b4b425e1aee387c0
snakerabbit <alison.s.cheng@gmail.com>
2017-11-06 11:45:04 -0800

    fixed footer links

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -6,8 +6,8 @@ export default () =>{
     <div className = 'footer'>
       <div className='test'>
         <a href='https://github.com/snakerabbit/jam' className = 'footer-links'>Github</a>
-        <a href='' className = 'footer-links'>LinkedIn</a>
-        <a href='' className = 'footer-links'>Facebook</a>
+        <a href='https://www.linkedin.com/in/alison-cheng/' className = 'footer-links'>LinkedIn</a>
+        <a href='http://www.alisoncheng.me/' className = 'footer-links'>Alison Cheng</a>
       </div>
     </div>
   );
